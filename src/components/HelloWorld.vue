da3301675444b34fca555ccc6a90365c1619493a
xurenkun <xurk@cyberpeace.cn>
2021-02-23 13:36:53 +0800

    升级为vite2，引入eslint校验代码格式，初始化项目配置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,58 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  {{ count }}
+  <button @click="sum"> 加加</button>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
-  }
-}
+<script lang="ts">
+  import { reactive, defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'HelloWorld',
+    props: {
+      msg: {
+        type: String,
+        required: true,
+      },
+    },
+    setup: () => {
+      const obj: any = {
+        name: 123,
+        wife: {
+          name: '美丽',
+        },
+        brother: ['大明', '小明', '明明'],
+      };
+      const count = reactive(obj);
+
+      const sum = () => {
+        count.brother[0] += '1231231';
+        count.brother.push('消极');
+        obj.sex = 'nan';
+        console.log(count);
+      };
+
+      return {
+        count,
+        sum,
+      };
+    },
+  });
 </script>
+
+<style scoped>
+  a {
+    color: #42b983;
+  }
+
+  label {
+    margin: 0 0.5em;
+    font-weight: bold;
+  }
+
+  code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+  }
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+const path = require('path')
+
+export default defineConfig({
+  plugins: [vue()],
+  alias: {
+    '@': path.resolve(__dirname, './src')
+  }
+})
